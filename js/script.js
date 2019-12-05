0678ebb8ed9a0a2d153cd0d44f2ed7ee31ab2567
Brad Davis <brad.davis.dev@gmail.com>
2019-12-04 21:17:36 -0500

    Add one possible way to refactor the loop

--- js/script.js
+++ js/script.js
@@ -4,10 +4,14 @@ var green;
 var blue;
 var rgbColor;
 
+function getRandColor() {
+    return Math.floor(Math.random() * 256 );
+}
+
 for (let i = 0; i < 10; i++) {
-    red = Math.floor(Math.random() * 256 );
-    green = Math.floor(Math.random() * 256 );
-    blue = Math.floor(Math.random() * 256 );
+    red = getRandColor();
+    green = getRandColor();
+    blue = getRandColor();
     rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
     html += '<div style="background-color:' + rgbColor + '"></div>';
 }
